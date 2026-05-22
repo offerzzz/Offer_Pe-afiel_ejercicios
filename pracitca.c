c76afdc08af11db4bb447cd2c8d4bdcc425be113
Offer Peñafiel <offer.penafiel@udla.edu.ec>
2026-05-22 14:54:11 -0500

    Apliación de size of y promedio de notas

--- pracitca.c
+++ pracitca.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 int main (){
-    float notas []=  {6,5 , 7.1, 6, 5, 8.1};
+    float notas []=  {6.5 , 7.1, 6, 5, 6.1};
+    int cantidad = sizeof(notas) / sizeof(notas[0]);
+    float suma= 0;
+    float promedio;
     for(int i = 0; i < 5; i++) {
         printf("Nota %d: %.1f\n", i + 1, notas[i]);
+        suma+= notas[i];
+
     }
+    //promedio=suma/5;
+    printf("promedio: %.2f\n", suma/5);
+
 
     return 0; 
 
